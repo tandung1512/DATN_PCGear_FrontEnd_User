81b0507580fd23bd5e339ec597ee3e8e420b683e
tandung1512 <tandung15122003@gmail.com>
2024-11-07 17:45:26 +0700

    add hompage

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -47,7 +47,7 @@ export class HeaderComponent implements OnInit {
   }
 
   // Navigate to User Info Update page
-  updateUserInfo(): void {
+  goToUserProfile() {
     this.router.navigate(['/user-info']);
   }
 
