e4b2bbf8bff8fbd31af3fabfb4cba08c6ec24718
tandung1512 <tandung15122003@gmail.com>
2024-11-14 22:10:43 +0700

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CartService } from './services/cart.service';
 import { CartComponent } from './cart/cart.component';
 
@@ -29,11 +29,7 @@ import { HttpClientModule } from '@angular/common/http';
     ReactiveFormsModule,
     FormsModule,
 
-    NgbModule,
     CartComponent,
-    
-
-  
 
   ],
   templateUrl: './app.component.html',  // Hoặc './product.component.html'
